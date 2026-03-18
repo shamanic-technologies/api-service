d781be16a52308abc46f9d3f2c5afdb2594275fe
KevinLourd <kevin.lourd@gmail.com>
2026-03-18 16:44:12 +0800

    Revert "feat: add leadsContacted field to campaign stats (#137)" (#138)
    This reverts commit f3e8f82f258b553aad132d9d7ee00bf6e97fdd8d.

--- src/lib/delivery-stats.ts
+++ src/lib/delivery-stats.ts
@@ -47,6 +47,5 @@ export async function fetchDeliveryStats(
     repliesNotInterested: b.repliesNotInterested || 0,
     repliesOutOfOffice: b.repliesOutOfOffice || 0,
     repliesUnsubscribe: b.repliesUnsubscribe || 0,
-    leadsContacted: b.recipients || 0,
   };
 }
